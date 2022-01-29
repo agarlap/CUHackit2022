e976c8651d6ffc356175428f264cd6b5e8d314b4
Joseph-Suter <90271974+Joseph-Suter@users.noreply.github.com>
2022-01-29 16:02:17 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,38 +4,69 @@
 //
 //  Created by Joseph Suter, Rich Bozard,  Aman Garlapati on 1/29/22.
 //
+//
+//  main.cpp
+//  CUhackit_plannter
+//
+//  Created by Joseph  Suter on 1/29/22.
+//
 #include <iostream>
-#include "Timer.h"
+#include "Header.h"
+#include <fstream>
 using namespace std;
 
+
+
+//Clock struct
 struct CLOCK {
     int hours;
     int minutes;
     int seconds;
 };
 
+//Tasks struct
+struct TASKS{
+    string title;
+    int priorty;
+    struct CLOCK duration;
+};
+
 void timeDifference(struct CLOCK, struct CLOCK, struct CLOCK *);
 
+
+
+
 int main() {
-    /*    // Sets Users Name
+    // Sets Users Name
     string userName;
     cout << "Please enter name: ";
     getline(cin, userName);
     cout << userName << endl;
-    */
     
     //Set Wake and Sleep times
-    
     struct CLOCK wakeup, sleep, activeHours;
     
     cout << "What time do you wake up (Hours min seconds): " << endl;
     cin >> wakeup.hours >> wakeup.minutes >> wakeup.seconds;
+    
     cout << "What time do you go to sleep (PM): " << endl;
     cin >> sleep.hours >> sleep.minutes >> sleep.seconds;
+    
     timeDifference (wakeup, sleep, &activeHours);
     
+    //Display activeHours
     cout << activeHours.hours << ":" << activeHours.minutes << ":" << activeHours.seconds;
     cout << endl;
+    
+    
+    //Task Array
+    int taskNum = 0;
+    TASKS array[taskNum];
+    
+    
+    //Menu
+    
+    
     
     /*
      //Timer inputs
