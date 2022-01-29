266b4c3c35f4d6e3ff8751a55998f7b1cb62cb65
Joseph-Suter <90271974+Joseph-Suter@users.noreply.github.com>
2022-01-29 12:30:54 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,31 @@
 //  Created by Joseph  Suter on 1/29/22.
 //
 #include <iostream>
+#include <chrono>
+#include <thread>
 using namespace std;
+
+void timer(int hours, int min, int sec);
+
+//Timer function
+void timer(int hours, int min, int sec){
+    for(;;){ //infinite loop
+        if(hours == 0 && min == 0 && sec == 0){
+            break;
+        }
+        if(sec == 0 && min == 0){
+            min = 60;
+            hours--;
+        }jj
+        if(sec == 0){
+            sec = 60;
+            min--;
+        }
+        this_thread::sleep_for(chrono::milliseconds(1000));
+        cout << hours << ":" << min << ":" << sec-- << endl;
+    }
+}
+
 int main(int argc, const char * argv[]) {
     
     // Sets Users Name
@@ -14,5 +38,17 @@ int main(int argc, const char * argv[]) {
     getline(cin, userName);
     cout << userName + "\n";
     
+    //Tester code
+    int hours, min, sec;
+    cout << "Please set a timer for the act:\n";
+    cout << "Hours: "; cin >> hours;
+    cout << "Minutes: "; cin >> min;
+    cout << "Seconds: ";  cin >> sec;
+    timer(hours, min, sec);
+    
+    
+    
+    
     return 0;
 }
+
