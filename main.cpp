faa6c618b5ec215b120e03cc1436c0d9d2538b37
Joseph-Suter <90271974+Joseph-Suter@users.noreply.github.com>
2022-01-29 11:06:56 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CUhackit_plannter
+//
+//  Created by Joseph  Suter on 1/29/22.
+//
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    
+    // Sets Users Name
+    string userName;
+    cout << "Please enter name: ";
+    getline(cin, userName);
+    cout << userName + "\n";
+    
+    return 0;
+}
