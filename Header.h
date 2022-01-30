f7c799bcd991c99d92279d0e874ff606e3ef5e4b
Joseph-Suter <90271974+Joseph-Suter@users.noreply.github.com>
2022-01-29 20:56:36 -0500

    Update Header.h

--- Header.h
+++ Header.h
@@ -25,9 +25,54 @@ void timer(int hours, int min, int sec){
     }
 }
 
-void menu (){
-    int taskNum = 0;
+
+struct CLOCK {
+    int hours;
+    int minutes;
+    int seconds;
+    string AMPM1;
+    string AMPM2;
+
+};
+
+void timeDifference(struct CLOCK, struct CLOCK, struct CLOCK *);
+
+void timeDifference(struct CLOCK wakeup, struct CLOCK sleep, struct CLOCK *activeHours){
     
-}
+    if(wakeup.AMPM1 != sleep.AMPM2){
+        sleep.hours += 12;
+    }
+    
+    if(wakeup.seconds > sleep.seconds){
+        --sleep.minutes;
+        sleep.seconds += 60;
+    }
+    activeHours->seconds = sleep.seconds - wakeup.seconds;
+    if(wakeup.minutes > sleep.minutes){
+        --sleep.hours;
+        sleep.minutes += 60;
+    }
+    activeHours->minutes = sleep.minutes - wakeup.minutes;
+    activeHours->hours = sleep.hours - wakeup.hours;
 }
 
+//Tasks struct
+struct TASKS{
+    string name;
+    string priorty;
+    string duration;
+    string currentDay;
+    string dueDay;
+};
+void menu(struct TASKS, struct TASKS, struct TASKS, struct TASKS, struct TASKS *);
+
+
+ int printMenu(int userPick){
+    cout << "MENU" << endl;
+    cout << "1. Create Task" << endl;
+    cout << "2. See Tasks" << endl;
+    cout << "3. Print Planner" << endl;
+    cout << "4. Quit" << endl;
+    cin >> userPick;
+    return userPick;
+}
